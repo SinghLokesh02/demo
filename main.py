f2bdb47374914dc7b4b92daa24554ad30af7d754
Lokesh <lokeshsingh7695@gmail.com>
2023-10-01 15:52:36 +0530

    first Project

---
+++ main.py
@@ -0,0 +1,15 @@
+print("This is my First code of Python")
+
+for i in range(0,11):
+    print(i)
+
+age = 18;
+print(f"My name is Lokesh Singh and my age is {age}")
+
+name = "lokesh singh"
+print(name.capitalize())
+print(name.upper())
+
+print(name.lower())
+
+print(name.islower())
